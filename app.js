0fb191b3c022057ec4ae5d20a869cb961e835a99
Deepanshu Narang <dn@hyathi.com>
2023-05-06 20:07:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 3000;
+const MONGODB_URI = "";
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define User schema
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  dob: Date,
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Parse incoming JSON requests
+app.use(bodyParser.json());
+
+// Handle GET request for all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Handle GET request for one user
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Handle POST request to create a new user
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Handle PUT request to update an existing user
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    user.set(req.body);
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Handle DELETE request to delete an existing user
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
